dfe80346cebb446db1e72337512c65bb8c6aaa1b
RonniePlay <reveinoff@gmail.com>
2025-07-25 16:39:38 +0300

    create product modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { ProductComponent } from './components/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [ProductComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'product-editor';
+  isShowModal: boolean = false;
+
+  setShowModal(): void {
+    this.isShowModal = true;
+  }
 }
