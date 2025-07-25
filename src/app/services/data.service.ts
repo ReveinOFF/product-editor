dbd67f3a62841480cbfae4bd871b36cccc4b42f1
RonniePlay <reveinoff@gmail.com>
2025-07-25 23:16:59 +0300

    Added json file and rxjs service

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,72 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Product {
+  id: string;
+  brand: string;
+  model_series: string;
+  device_pn: string;
+  dev_type: string;
+  rnk: number;
+  min_price: number;
+  max_price: number;
+  quantity: number;
+  ePrq: number;
+  eA4q: number;
+  sku: string;
+  l_user: string;
+  category: string;
+  e_user: string;
+  green_id: string;
+  edit_at: string;
+  create_at: string;
+  amazon_listing_src: string;
+  elCcq: number;
+  amazq: number;
+  notes: string;
+  photo_device: string;
+  photo_data_plate: string;
+  location: Location[];
+  ebay_listings: EbayListing[];
+  same_products: SameProduct[];
+}
+
+export interface Location {
+  id: string;
+  brand: string;
+  category: string;
+  device_pn: string;
+  qty: number;
+  condition: string;
+  edit_user: string;
+  notes: string;
+}
+
+export interface EbayListing {
+  listing_id: string;
+  sku: string;
+  condition_id: string;
+  product_id: string;
+  title: string;
+  price: number;
+  listing_qty: number;
+  inventory_qty: number;
+}
+
+export interface SameProduct {
+  original_id: string;
+  same_id: string;
+  fk_edit_employee: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  constructor(private http: HttpClient) {}
+
+  getData(): Observable<Product> {
+    return this.http.get<Product>('assets/data/items.json');
+  }
+}
